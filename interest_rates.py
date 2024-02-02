932101f891fe0aab09295aa7d2eaac5d2e200db8
Matt <115906976+uthberg@users.noreply.github.com>
2024-02-01 17:12:32 -0800

    Update interest_rates.py

--- interest_rates.py
+++ interest_rates.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python
 
-deposit = 100
+deposit = 100 #how much money you start with
 
-apy = 5 / 100
+apy = 5 / 100 #5.00 percent annual percentage yield
 
-years = 5
+years = 5 #over 5 year time frame
 
 interest = deposit * apy
 print("Interest:", interest)
@@ -15,6 +15,6 @@ compound_interest = earnings * apy
 print("Interest made after one year:", compound_interest)
 
 compound_interest_final = compound_interest * years
-print("Compound interest made after (x) years:", compound_interest_final + deposit)
+print("Compound interest made after 5 years:", compound_interest_final + deposit)
 
 
